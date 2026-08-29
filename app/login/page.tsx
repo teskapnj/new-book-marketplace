d0cebf0b6905e1e4d7253c4086d37d62b1b08c88
bahakaba <57778488+bahakaba@users.noreply.github.com>
2026-08-29 14:38:20 -0400

    Remove signup link from login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -738,14 +738,7 @@ export default function LoginPage() {
             onError={handleSocialLoginError}
           />
         </div>
-        
-        {/* Sign Up Link */}
-        <p className="text-center text-sm text-gray-600">
-          Don't have an account?{' '}
-          <Link href="/register" className="font-medium text-blue-600 hover:text-blue-500">
-            Sign up
-          </Link>
-        </p>
+       
         
         {/* Footer */}
         <div className="mt-8 text-center text-xs text-gray-500">
