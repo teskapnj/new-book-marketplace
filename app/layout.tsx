726c8d5642808680a034b434366bc9184a69040b
bahakaba <57778488+bahakaba@users.noreply.github.com>
2026-09-11 18:16:20 -0400

    Explicitly set favicon metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,11 @@ export const metadata: Metadata = {
   creator: 'SellBook Media',
   publisher: 'SellBook Media',
 
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+
   robots: {
     index: true,
     follow: true,
